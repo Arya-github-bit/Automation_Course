daf91ef95678976768777700d1e711cafb109afd
Radhi <Radhi@192.168.1.6>
2025-01-05 21:07:22 +0530

    God bless you

---
+++ AutomationCourse/src/test/java/testscript/Base.java
@@ -0,0 +1,31 @@
+package testscript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	public WebDriver driver ;
+
+	public void initialiseBrowser()
+	{
+		driver = new ChromeDriver();//lauching a Chrome browser
+		driver.get("https://selenium.qabible.in/simple-form-demo.php");//open amazon website
+		driver.manage().window().maximize();//to maximize window
+
+	}
+	public void browserQuit()
+	{
+		driver.quit();//browser session terminate//commonly used
+		//driver.close();//only specific tab  closes
+	}
+
+
+	public static void main(String[] args) {
+		Base base = new Base();
+		base.initialiseBrowser();
+		base.browserQuit();
+
+	}
+
+
+}
