2873a06e6ce9f31b830d15300f3ae14aa9e9cd1e
Radhi <Radhi@192.168.1.6>
2025-01-07 08:10:40 +0530

    7/01/2024

--- AutomationCourse/src/test/java/testscript/BrowserCommands.java
+++ AutomationCourse/src/test/java/testscript/BrowserCommands.java
@@ -1,11 +1,13 @@
 package testscript;
-
 public class BrowserCommands extends Base {
-	
-	 public void browserCommands()
-	 {
-		 String title = driver.getTitle();
-	 }
+
+	public void browserCommands()
+	{
+		String title = driver.getTitle();// get title of webpage
+		String url = driver.getCurrentUrl();// get url of browser
+		 String pagesource = driver.getPageSource();//get page source of page source
+		 
+	}
 	public static void main(String[] args) {
 		BrowserCommands browsercommands = new BrowserCommands();
 		browsercommands.initialiseBrowser();
