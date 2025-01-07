cdeeb1532d7fee00c37d6b31325d2f848eaed958
Radhi <Radhi@192.168.1.6>
2025-01-07 19:47:46 +0530

    7/01/2024

---
+++ AutomationCourse/src/test/java/testscript/WebElementCommands.java
@@ -0,0 +1,14 @@
+package testscript;
+public class WebElementCommands extends Base {
+	public void locators()
+	{
+
+	}
+	public static void main(String[] args) {
+		WebElementCommands webelementcommands = new WebElementCommands();
+		webelementcommands.locators();
+		webelementcommands.initialiseBrowser();
+		webelementcommands.browserQuit();
+	}
+
+}
