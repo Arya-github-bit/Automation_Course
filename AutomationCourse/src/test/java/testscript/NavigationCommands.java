b637cc575cfad9f71a446ccd3281b9fd24ac7e2b
Radhi <Radhi@192.168.1.6>
2025-01-13 06:36:00 +0530

    13/01/2025

--- AutomationCourse/src/test/java/testscript/NavigationCommands.java
+++ AutomationCourse/src/test/java/testscript/NavigationCommands.java
@@ -3,10 +3,10 @@ package testscript;
 public class NavigationCommands extends Base {
 	public void navigators()
 	{
-		driver.navigate().to("https://www.amazon.in");
-        driver.navigate().back();
-        driver.navigate().forward();
-        driver.navigate().refresh();
+		driver.navigate().to("https://www.amazon.in");//navigate to amazon web page
+        driver.navigate().back();//navigate back to obsqura homepage
+        driver.navigate().forward();//navigate to amazon web page
+        driver.navigate().refresh();//refresh page
 	}
 	public static void main(String[] args) {
 
